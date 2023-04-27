9990d7ff538c7b246791593bab62d47f5d0686a3
zulfianprasetyo <you@example.com>
2023-04-27 21:03:22 +0700

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://zulfian506150700:o31kflJMpWkJSl43@cluster0.ksi9p44.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -21,7 +33,7 @@ def bucket_post():
         'done': 0
     }
     db.bucket.insert_one(doc)
-    return jsonify({'msg':'Data saved!'})
+    return jsonify({'msg':'Data Berhasil Tersimpan!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
@@ -30,7 +42,7 @@ def bucket_done():
         {'num': int(num_receive)},
         {'$set': {'done': 1}}
     )
-    return jsonify({'msg': 'Update done!'})
+    return jsonify({'msg': 'Update Berhasil Dimasukkan!'})
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
@@ -41,7 +53,7 @@ def bucket_get():
 def bucket_delete():
     num_receive = request.form['num_give']
     db.bucket.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': 'Delete done!'})
+    return jsonify({'msg': 'Delete Selesai'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5001, debug=True)
